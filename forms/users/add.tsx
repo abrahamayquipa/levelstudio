d77ca933621999ce6db9c30391b90c21cf0986a7
abrancitouu <abrahamayquipa2000@gmail.com>
2025-01-27 15:58:30 -0500

    feat: add all the validations

--- forms/users/add.tsx
+++ forms/users/add.tsx
@@ -1,31 +1,77 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useCreateUser } from "@levelstudio/hooks";
+import { z } from "zod";
 import {
     Button,
     Form,
     FormControl,
     FormField,
     FormItem,
+    FormMessage,
     Input,
 } from "@levelstudio/components/ui";
 import { useDialog } from "@levelstudio/contexts/dialog";
+import {
+    useGetUsers,
+    useCreateUser
+} from "@levelstudio/hooks";
+
+const userSchema = z.object({
+    name: z
+        .string()
+        .regex(/^[A-Za-z\s]+$/, "El nombre solo puede contener letras y espacios.")
+        .min(1, "El nombre es obligatorio."),
+    lastname: z
+        .string()
+        .regex(/^[A-Za-z\s]+$/, "El apellido solo puede contener letras y espacios.")
+        .min(1, "El apellido es obligatorio."),
+    age: z
+        .string()
+        .transform((val) => parseInt(val, 10))
+        .refine((val) => !isNaN(val), {
+            message: "La edad debe ser un número válido.",
+        })
+        .refine((val) => val > 0 && val < 80, {
+            message: "La edad debe estar entre 0 y 80.",
+        }),
+});
 
 export const AddUser = () => {
     const { closeDialog } = useDialog();
     const [createUser, loading, error] = useCreateUser();
+    const [users, usersLoading] = useGetUsers();
 
     const form = useForm({
+        resolver: zodResolver(userSchema),
         defaultValues: {
             name: "",
             lastname: "",
-            age: ""
+            age: "",
         },
     });
 
-    const onSubmit = (values: any) => {
-        createUser(values);
+    const onSubmit = async (values: any) => {
+        const userExists = users.some(
+            (user) =>
+                user.name.toLowerCase() === values.name.toLowerCase() &&
+                user.lastname.toLowerCase() === values.lastname.toLowerCase()
+        );
+
+        if (userExists) {
+            form.setError("name", {
+                type: "manual",
+                message: "Ya existe un usuario con este nombre y apellido.",
+            });
+            form.setError("lastname", {
+                type: "manual",
+                message: "Ya existe un usuario con este nombre y apellido.",
+            });
+            return;
+        }
+
+        await createUser(values);
         closeDialog();
     };
 
@@ -35,10 +81,10 @@ export const AddUser = () => {
                 <FormField
                     control={form.control}
                     name="name"
-                    render={({ field }) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-8 bg-transparent">
                             <label
-                                htmlFor="nombre"
+                                htmlFor="name"
                                 className="mb-4 font-light text-[#a5a5a5]"
                             >
                                 Nombre
@@ -50,16 +96,23 @@ export const AddUser = () => {
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage>
+                                {fieldState.error?.message && (
+                                    <span className="text-sm text-red-500">
+                                        {fieldState.error.message}
+                                    </span>
+                                )}
+                            </FormMessage>
                         </FormItem>
                     )}
                 />
                 <FormField
                     control={form.control}
                     name="lastname"
-                    render={({ field }) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-8 bg-transparent">
                             <label
-                                htmlFor="nombre"
+                                htmlFor="lastname"
                                 className="mb-4 font-light text-[#a5a5a5]"
                             >
                                 Apellidos
@@ -71,27 +124,39 @@ export const AddUser = () => {
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage>
+                                {fieldState.error?.message && (
+                                    <span className="text-sm text-red-500">
+                                        {fieldState.error.message}
+                                    </span>
+                                )}
+                            </FormMessage>
                         </FormItem>
                     )}
                 />
                 <FormField
                     control={form.control}
                     name="age"
-                    render={({ field }) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-12 bg-transparent">
-                            <label
-                                htmlFor="nombre"
-                                className="mb-4 font-light text-[#a5a5a5]"
-                            >
+                            <label htmlFor="age" className="mb-4 font-light text-[#a5a5a5]">
                                 Edad
                             </label>
                             <FormControl>
                                 <Input
+                                    type="number"
                                     className="font-light placeholder:text-[#a5a5a5] rounded-xl border-0 focus:outline-none focus:ring-0"
                                     placeholder="Escribe tu edad"
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage>
+                                {fieldState.error?.message && (
+                                    <span className="text-sm text-red-500">
+                                        {fieldState.error.message}
+                                    </span>
+                                )}
+                            </FormMessage>
                         </FormItem>
                     )}
                 />
--- forms/users/edit.tsx
+++ forms/users/edit.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useUpdateUser } from "@levelstudio/hooks";
+import { z } from "zod";
 import {
     Button,
     Form,
@@ -11,22 +12,67 @@ import {
     Input,
 } from "@levelstudio/components/ui";
 import { useDialog } from "@levelstudio/contexts/dialog";
+import {
+    useGetUsers,
+    useUpdateUser
+} from "@levelstudio/hooks";
+
+const userSchema = z.object({
+    id: z.string(),
+    name: z
+        .string()
+        .regex(/^[A-Za-z\s]+$/, "El nombre solo puede contener letras y espacios.")
+        .min(1, "El nombre es obligatorio."),
+    lastname: z
+        .string()
+        .regex(/^[A-Za-z\s]+$/, "El apellido solo puede contener letras y espacios.")
+        .min(1, "El apellido es obligatorio."),
+    age: z
+        .string()
+        .transform((val) => parseInt(val, 10))
+        .refine((val) => !isNaN(val), {
+            message: "La edad debe ser un número válido.",
+        })
+        .refine((val) => val > 0 && val < 80, {
+            message: "La edad debe estar entre 0 y 80.",
+        }),
+});
 
 export const EditUser = ({ initialValues }: any) => {
     const { closeDialog } = useDialog();
     const [updateUser, loading, error] = useUpdateUser();
+    const [users, usersLoading] = useGetUsers();
 
     const form = useForm({
+        resolver: zodResolver(userSchema),
         defaultValues: {
             id: initialValues.id,
             name: initialValues.name,
             lastname: initialValues.lastname,
-            age: initialValues.age,
-        }
+            age: String(initialValues.age),
+        },
     });
 
-    const onSubmit = (values: any) => {
-        updateUser(values);
+    const onSubmit = async (values: any) => {
+        const userExists = users.some(
+            (user) =>
+                user.name.toLowerCase() === values.name.toLowerCase() &&
+                user.lastname.toLowerCase() === values.lastname.toLowerCase()
+        );
+
+        if (userExists) {
+            form.setError("name", {
+                type: "manual",
+                message: "Ya existe un usuario con este nombre y apellido.",
+            });
+            form.setError("lastname", {
+                type: "manual",
+                message: "Ya existe un usuario con este nombre y apellido.",
+            });
+            return;
+        }
+
+        await updateUser(values);
         closeDialog();
     };
 
@@ -36,31 +82,36 @@ export const EditUser = ({ initialValues }: any) => {
                 <FormField
                     control={form.control}
                     name="name"
-                    render={({field}) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-8 bg-transparent">
                             <label
-                                htmlFor="nombre"
+                                htmlFor="name"
                                 className="mb-4 font-light text-[#a5a5a5]"
                             >
                                 Nombre
                             </label>
                             <FormControl>
-                            <Input
+                                <Input
                                     className="font-light placeholder:text-[#a5a5a5] rounded-xl border-0 focus:outline-none focus:ring-0"
                                     placeholder="Escribe tu nombre"
                                     {...field}
                                 />
                             </FormControl>
+                            {fieldState.error?.message && (
+                                <span className="text-sm text-red-500">
+                                    {fieldState.error.message}
+                                </span>
+                            )}
                         </FormItem>
                     )}
                 />
                 <FormField
                     control={form.control}
                     name="lastname"
-                    render={({field}) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-8 bg-transparent">
                             <label
-                                htmlFor="nombre"
+                                htmlFor="lastname"
                                 className="mb-4 font-light text-[#a5a5a5]"
                             >
                                 Apellidos
@@ -72,27 +123,35 @@ export const EditUser = ({ initialValues }: any) => {
                                     {...field}
                                 />
                             </FormControl>
+                            {fieldState.error?.message && (
+                                <span className="text-sm text-red-500">
+                                    {fieldState.error.message}
+                                </span>
+                            )}
                         </FormItem>
                     )}
                 />
                 <FormField
                     control={form.control}
                     name="age"
-                    render={({field}) => (
+                    render={({ field, fieldState }) => (
                         <FormItem className="mb-12 bg-transparent">
-                            <label
-                                htmlFor="nombre"
-                                className="mb-4 font-light text-[#a5a5a5]"
-                            >
+                            <label htmlFor="age" className="mb-4 font-light text-[#a5a5a5]">
                                 Edad
                             </label>
                             <FormControl>
-                            <Input
+                                <Input
+                                    type="number"
                                     className="font-light placeholder:text-[#a5a5a5] rounded-xl border-0 focus:outline-none focus:ring-0"
                                     placeholder="Escribe tu edad"
                                     {...field}
                                 />
                             </FormControl>
+                            {fieldState.error?.message && (
+                                <span className="text-sm text-red-500">
+                                    {fieldState.error.message}
+                                </span>
+                            )}
                         </FormItem>
                     )}
                 />
